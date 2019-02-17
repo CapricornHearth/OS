8d4cd95ce2fc60635c6d5ba0d061dd2615f1106e
致远 <34001987+CapricornHearth@users.noreply.github.com>
2019-02-17 15:07:59 +0800

    Add files via upload

---
+++ 首次适应算法/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.N = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.End = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.S = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Add = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.End1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.N,
+            this.A,
+            this.End,
+            this.S});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(796, 156);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // N
+            // 
+            this.N.HeaderText = "Name";
+            this.N.Name = "N";
+            this.N.Width = 200;
+            // 
+            // A
+            // 
+            this.A.HeaderText = "Start";
+            this.A.Name = "A";
+            this.A.Width = 175;
+            // 
+            // End
+            // 
+            this.End.HeaderText = "End";
+            this.End.Name = "End";
+            this.End.Width = 175;
+            // 
+            // S
+            // 
+            this.S.HeaderText = "Size";
+            this.S.Name = "S";
+            this.S.Width = 200;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Add,
+            this.End1,
+            this.SI});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(794, 150);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.HeaderText = "Start";
+            this.Add.Name = "Add";
+            this.Add.Width = 190;
+            // 
+            // End1
+            // 
+            this.End1.HeaderText = "End";
+            this.End1.Name = "End1";
+            this.End1.Width = 190;
+            // 
+            // SI
+            // 
+            this.SI.HeaderText = "Size";
+            this.SI.Name = "SI";
+            this.SI.Width = 370;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(299, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "未分配内存表";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(299, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "已分配内存表";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(105, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(373, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Size";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(169, 457);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(441, 457);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(287, 516);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(352, 516);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(599, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "创建作业";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(508, 516);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "删除作业";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(352, 407);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(232, 407);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "初始内存大小";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(508, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "初始化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 560);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N;
+        private System.Windows.Forms.DataGridViewTextBoxColumn A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn End;
+        private System.Windows.Forms.DataGridViewTextBoxColumn S;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Add;
+        private System.Windows.Forms.DataGridViewTextBoxColumn End1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SI;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 首次适应算法/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public struct Nodes {
+            public int name, size, start, end;
+            public bool state;
+            public Nodes(int start, int end, int size,int name=0,  bool state=true)
+            {
+                //name为0和state为true表示内存未分配
+                this.name = name;   //任务编号
+                this.start = start;   
+                this.end = end;
+                this.size = size;
+                this.state = state;
+            }
+        }
+        private List<Nodes> AllocList = new List<Nodes>();        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "基于空闲分区链的首次适应算法模拟程序";
+        }
+        private bool judge(List<Nodes> a,int name)
+        {
+            bool s = false;
+            for (int i = 0; i < a.Count(); i++)
+            {
+                if (a[i].name == name)
+                {
+                    MessageBox.Show("该编号任务已分配！");
+                    return true;
+                }
+            }
+            return false;
+        }
+        //分配
+        private void alloc(ref List<Nodes> a,int taskId,int taskLength)
+        {
+            for (int i = 0; i < a.Count(); i++)
+            {
+                Nodes p = a[i];
+                if (p.state == true & p.size > taskLength)
+                {
+                    Nodes q1 = new Nodes(p.start + taskLength, p.end, p.size-taskLength, 0, true);   //空闲块
+                    Nodes q2 = new Nodes(p.start, p.start + taskLength - 1, taskLength, taskId, false); //已分配块
+                    a.RemoveAt(i);
+                    a.Insert(i, q1);
+                    a.Insert(i, q2);
+                    return;
+
+                }
+                if (p.state == true & p.size == taskLength)
+                {
+                    p.state = false;
+                    a[i] = p;
+                    return;
+                }
+            }
+            MessageBox.Show("内存空间不足！");
+        }
+        //释放并合并空闲分区块
+        private void free(ref List<Nodes> a,int taskid)
+        {
+            int x=0;
+            for (int i = 0; i < a.Count(); i++)
+            {
+                Nodes p=a[i];
+                if (p.name == taskid)
+                {
+                    p.state = true;
+                    p.name = 0;
+                    a[i] = p;
+                    x = i;
+                    break;
+                }
+            }
+            //向前合并
+            if (x - 1 >=0)
+            {
+                if(a[x-1].state==true)
+                {
+                    Nodes q = new Nodes(a[x - 1].start, a[x].end, a[x - 1].size + a[x].size, 0, true);
+                    a.RemoveAt(x - 1);
+                    a.RemoveAt(x - 1);
+                    a.Insert(x - 1, q);
+                    x = x - 1;
+                }
+            }
+            //向后合并
+            if(x+1<a.Count())
+            {
+                if (a[x + 1].state == true)
+                {
+                    Nodes q = new Nodes(a[x].start, a[x + 1].end, a[x].size + a[x + 1].size, 0, true);
+                    a.RemoveAt(x );
+                    a.RemoveAt(x);
+                    a.Insert(x, q);
+                }
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //创建作业
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int name = Convert.ToInt32(textBox1.Text);
+            int size = Convert.ToInt32(textBox2.Text);
+            if (judge(AllocList, name))
+            {
+                return;
+            }
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            alloc(ref AllocList, name, size);
+            for(int i=0;i<AllocList.Count();i++)
+            {
+                if (AllocList[i].state == false)
+                {
+                    string name1 = Convert.ToString(AllocList[i].name);
+                    string start = Convert.ToString(AllocList[i].start);
+                    string end = Convert.ToString(AllocList[i].end);
+                    string size1 = Convert.ToString(AllocList[i].size);
+                    dataGridView1.Rows.Add(new string[] { name1, start,end, size1 });
+                }
+                if (AllocList[i].state == true)
+                {
+                    string start = Convert.ToString(AllocList[i].start);
+                    string end = Convert.ToString(AllocList[i].end);
+                    string size1 = Convert.ToString(AllocList[i].size);
+                    dataGridView2.Rows.Add(new string[] {start, end, size1 });
+                }
+            }
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //删除作业
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int namedel = Convert.ToInt32(textBox3.Text);
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            free(ref AllocList, namedel);
+            for (int i = 0; i < AllocList.Count(); i++)
+            {
+                if (AllocList[i].state == false)
+                {
+                    string name1 = Convert.ToString(AllocList[i].name);
+                    string start = Convert.ToString(AllocList[i].start);
+                    string end = Convert.ToString(AllocList[i].end);
+                    string size1 = Convert.ToString(AllocList[i].size);
+                    dataGridView1.Rows.Add(new string[] { name1, start, end, size1 });
+                }
+                if (AllocList[i].state == true)
+                {
+                    string start = Convert.ToString(AllocList[i].start);
+                    string end = Convert.ToString(AllocList[i].end);
+                    string size1 = Convert.ToString(AllocList[i].size);
+                    dataGridView2.Rows.Add(new string[] { start, end, size1 });
+                }
+            }
+        }
+        //初始化
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int ss = Convert.ToInt32(textBox4.Text);
+            Nodes a = new Nodes(0, ss - 1, ss, 0, true);
+            AllocList.Add(a);
+            dataGridView2.Rows.Add(new string[] { "0", Convert.ToString(ss - 1), Convert.ToString(ss) });
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
